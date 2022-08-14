22a086baa4fbd5998cd9617338b05c656c591cf4
Stone Comstock <comstockstone@yahoo.com>
2022-08-14 16:46:30 -0700

    setting up states on app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,13 +5,20 @@ import Playlist from '../Playlist/Playlist';
 import { useState, useEffect } from 'react';
 
 const App = () => {
+  // setting states for searchresults, playlistname, and playlist tracks.
+  // will be passed down to components that render them.
   const [searchResults, setSearchResults] = useState([
     { name: 'name', artist: 'artist', album: 'album', id: 'id' },
     { name: 'name2', artist: 'artist2', album: 'album2', id: 'id2' },
   ]);
-useEffect(() => {
-  console.log(searchResults)
-}, []);
+  const [playlistName, setPlaylistName] = useState('My Playlist');
+  const [playlistTracks, setPlaylistTracks] = useState([
+    { name: 'name3', artist: 'artist3', album: 'album3', id: 'id3' },
+    { name: 'name4', artist: 'artist4', album: 'album4', id: 'id4' },
+  ]);
+  useEffect(() => {
+    console.log(searchResults);
+  }, []);
   return (
     <div>
       <h1>
